12dad5e8bc5952900116d06dd23b3ca33607ca41
DanialBillows <danialbillows@icloud.com>
2025-11-11 11:08:19 +0100

    Visual studio solution created

---
+++ FoutloosTypen/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FoutloosTypen")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FoutloosTypen.Pages")]
